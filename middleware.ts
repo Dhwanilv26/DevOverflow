48a22de0d2035d1b8655462897a1dc7a9ab29b96
Dhwanil <dhwanil2604@gmail.com>
2024-10-17 19:36:47 +0530

    corrected clerk routes issue and implemented basic ask a question form

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/"]);
+// Define the public routes
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
+
+// Define the ignored routes
+const isIgnoredRoute = createRouteMatcher(["/api/webhook", "/api/chatgpt"]);
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
     auth().protect();
   }
 });
-
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
