60095dac426a69ef6d239ce38b1829ae34234574
Dhwanil <dhwanil2604@gmail.com>
2024-11-05 23:16:30 +0530

    implemented local search completely

--- app/(root)/tags/[id]/page.tsx
+++ app/(root)/tags/[id]/page.tsx
@@ -1,8 +1,6 @@
 import QuestionCard from '@/components/cards/QuestionCard';
 import NoResult from '@/components/shared/NoResult';
 import LocalSearchBar from '@/components/shared/search/LocalSearchBar';
-import { QuestionFilters } from '@/constants/filters';
-import { IQuestion } from '@/database/question.model';
 import { getQuestionsByTagId } from '@/lib/actions/tag.actions'
 import { URLProps } from '@/types';
 import React from 'react'
